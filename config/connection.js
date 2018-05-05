b4c82caff70d0424a78cab1a2d8dfa8c71dee342
Paula MacTavish <perilousp@hotmail.com>
2018-05-05 01:00:19 -0400

    add heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 
 var mysql = require("mysql");
-
+var connection;
 // ************************* CONNECT TO MYSQL *************************
-var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+
+    connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
