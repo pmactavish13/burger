afe42776846433de594829de0716bb51664c3438
Paula MacTavish <perilousp@hotmail.com>
2018-05-05 13:11:43 -0400

    heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 -- Insert a set of records.
 INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger",false),
-("Portabello Muchroom Burger", false),
+("Portabello Mushroom Burger", false),
 ("Alvacado Provolone Burger", false);
 
 SELECT*FROM burgers;
